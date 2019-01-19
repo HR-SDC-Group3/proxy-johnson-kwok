fe4a20dd9bb66b2c224fba85684ee9c905ed0735
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-19 12:01:20 -0800

    Update k6 script

--- server/k6Script.js
+++ server/k6Script.js
@@ -3,17 +3,17 @@ import { sleep, check } from 'k6';
 
  export const options = {
   vus: 100,
-  rps: 1,
+  rps: 1000,
   duration: '5m',
 };
 
  export default function() {
-  const randomId = Math.floor(Math.random() * (10000000 - 9000000 + 1)) + 9000000;
-  const response = http.get(`http://localhost:3000/api/restaurants/${randomId}/reviews`);    // reviews service
-  // const response = http.post(`http://localhost:3000/api/restaurants/${randomId}/reviews`);      // reviews service
+  const randomId = Math.floor(Math.random() * (10000000 - 8000000 + 1)) + 8000000;
+  // const response = http.get(`http://localhost:3000/api/restaurants/${randomId}/reviews`);    // reviews proxy
+  const response = http.post(`http://localhost:3000/api/restaurants/${randomId}/reviews`);      // reviews proxy
   check(response, {
-    "status was 200": (r) => r.status == 200,    // for GET requests
-    // "status was 201": (r) => r.status == 201,       // for POST requests
+    // "status was 200": (r) => r.status == 200,    // for GET requests
+    "status was 201": (r) => r.status == 201,       // for POST requests
     "transaction time OK": (r) => r.timings.duration < 2000
   });
   // sleep(0.05);
