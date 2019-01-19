5c0c3a31750c7781dfc437b1a71627af3959481c
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-18 21:33:51 -0800

    Change http proxy middleware target to localhost
    * Add New Relic and k6 script

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
+require('newrelic');
 const express = require('express');
-const bodyParser = require('body-parser');
-const proxy = require('http-proxy-middleware')
+const proxy = require('http-proxy-middleware');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('tiny'));
 app.use('/restaurants/:id/', express.static(`${__dirname}/../client/dist/`));
-app.use(bodyParser());
 
-app.use('/api/reservations/restaurantID=:restaurantID&date=:date', proxy({ target: 'http://54.183.92.72:3002', changeOrigin: true }));
-app.use('/api/reservations/', proxy({ target: 'http://54.183.92.72:3002', changeOrigin: true }));
-app.use('/api/restaurants/:id/reviews', proxy({ target: 'http://54.183.92.72:3004', changeOrigin: true }));
-app.use('/api/restaurants/:id/menu', proxy({ target: 'http://ec2-13-57-40-25.us-west-1.compute.amazonaws.com', changeOrigin: true }));
-app.use('/api/restaurants/:id/photos', proxy({ target: 'http://ec2-18-222-226-95.us-east-2.compute.amazonaws.com', changeOrigin: true }));
+// app.use('/api/reservations/restaurantID=:restaurantID&date=:date', proxy({ target: 'http://54.183.92.72:3002', changeOrigin: true }));
+// app.use('/api/reservations/', proxy({ target: 'http://54.183.92.72:3002', changeOrigin: true }));
+app.use('/api/restaurants/:id/reviews', proxy({ target: 'http://localhost:3004', changeOrigin: true }));
+// app.use('/api/restaurants/:id/menu', proxy({ target: 'http://ec2-13-57-40-25.us-west-1.compute.amazonaws.com', changeOrigin: true }));
+// app.use('/api/restaurants/:id/photos', proxy({ target: 'http://ec2-18-222-226-95.us-east-2.compute.amazonaws.com', changeOrigin: true }));
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
