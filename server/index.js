cb862f24bf5d9c2f5b0536d8919a2d54e02c7845
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-23 21:31:54 -0800

    Change hosts for deployment

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ const app = express();
 app.use(morgan('tiny'));
 app.use('/restaurants/:id/', express.static(`${__dirname}/../client/dist/`));
 
-// app.use('/api/reservations/restaurantID=:restaurantID&date=:date', proxy({ target: 'http://54.183.92.72:3002', changeOrigin: true }));
-// app.use('/api/reservations/', proxy({ target: 'http://54.183.92.72:3002', changeOrigin: true }));
-app.use('/api/restaurants/:id/reviews', proxy({ target: 'http://localhost:3004', changeOrigin: true }));
-// app.use('/api/restaurants/:id/menu', proxy({ target: 'http://ec2-13-57-40-25.us-west-1.compute.amazonaws.com', changeOrigin: true }));
-// app.use('/api/restaurants/:id/photos', proxy({ target: 'http://ec2-18-222-226-95.us-east-2.compute.amazonaws.com', changeOrigin: true }));
+// app.use('/api/reservations/restaurantID=:restaurantID&date=:date', proxy({ target: 'http://18.144.29.119:3002', changeOrigin: true }));
+// app.use('/api/reservations/', proxy({ target: 'http://18.144.29.119:3002', changeOrigin: true }));
+app.use('/api/restaurants/:id/reviews', proxy({ target: 'http://13.57.23.184:3004', changeOrigin: true }));
+// app.use('/api/restaurants/:id/menu', proxy({ target: 'http://18.218.178.24:3001', changeOrigin: true }));
+// app.use('/api/restaurants/:id/photos', proxy({ target: 'http://54.153.94.82:3003', changeOrigin: true }));
 
 const port = 3000;
 app.listen(port, () => {
